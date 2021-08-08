8ebcb5419964042bf9d3bfb2715c8746ab56afa8
roxyluoxiaofei <roxyluoxiaofei@gmail.com>
2021-08-08 14:38:50 -0700

    Navbar, routing links, and notFound page added

--- timesheet-frontend/src/App.js
+++ timesheet-frontend/src/App.js
@@ -1,13 +1,32 @@
+import React, { Component } from "react";
 import './App.css';
 import TimeSheetHome from './components/TimeSheetHome/TimeSheetHome';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from './components/navbar/navbar';
+import Summary from './components/summary/summary';
+import NotFound from "./components/summary/notFound";
 
-function App() {
-  return (
-    <div className="TimeSheetHome">
-      <TimeSheetHome></TimeSheetHome>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/summary" component={Summary} />
+            <Route path="/timesheet" component={TimeSheetHome} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/summary" />
+            <Redirect to="/not-found"/>
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
+
---
+++ timesheet-frontend/src/components/summary/summary.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Summary extends Component {
+    state = {  }
+    render() { 
+        return (
+            <span>this is summary</span>
+         );
+    }
+}
+ 
+export default Summary;
