334d2cd959c5b541f9c501c01799bfca3fe55116
Phuong La <35988387+Gutalala@users.noreply.github.com>
2021-08-08 15:16:16 -0600

    Added Store and Router

--- timesheet-frontend/src/components/TimeSheetHome/TimeSheetHome.js
+++ timesheet-frontend/src/components/TimeSheetHome/TimeSheetHome.js
@@ -1,110 +1,9 @@
-import React from 'react';
+import {React, useState} from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
-
-
-// const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-var timesheet = [{
-    "day" : "Sunday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{fasudfnaindfuianiufanisn
-    "day" : "Monday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{
-    "day" : "Tuesday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{
-    "day" : "Wednesday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{
-    "day" : "Thursday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{
-    "day" : "Friday",
-    "date": "2021-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T07:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-},
-{
-    "day" : "Saturday",
-    "date": "2025-01-17T07:00:00.000Z",
-    "startingTime": "2021-01-17T07:00:00.000Z",
-    "endingTime": "2021-01-17T14:00:00.000Z",
-    "isFloatingDay": false,
-    "isHoliday": false,
-    "isVacation": false
-}
-
-]
-
-
-const columns = [{
-    dataField: 'day',
-    text: 'Week Day'
-  },
-{
-    dataField: 'date',
-    text: 'Date'
-},
-{
-    dataField: 'startingTime',
-    text: 'Starting Time'
-},
-{
-    dataField: 'endingTime',
-    text: 'Ending Time'
-},
-{
-    dataField: 'startingTime',
-    text: 'Total Hour',
-    formatter: totalHoursCalc
-},
-{
-    dataField: 'isFloatingDay',
-    text: 'Floating Day',
-},
-{
-    dataField: 'isHoliday',
-    text: 'Holiday',
-},
-{
-    dataField: 'isVacation',
-    text: 'Vacation',
-},
-];
+import { TextField, Checkbox, Button } from '@material-ui/core';
+import { connect } from 'react-redux';
+import * as actionTypes from '../../action/actionTypes';
+import TimeSheetHomeCSS from './TimeSheetHome.module.css';
 
 function totalHoursCalc(cell, row){
     var timeStart = new Date(row.startingTime).getHours();
@@ -112,14 +11,112 @@ function totalHoursCalc(cell, row){
     var hourDiff = timeEnd - timeStart;
     return  (
      <span>
-        {hourDiff} = {timeEnd} - {timeStart}
+        {hourDiff}
     </span>)
 }
 
-export default function TimeSheetHome() {
+
+
+function TimeSheetHome(props) {
+
+    const [timesheet, setTimesheet] = useState(props.timesheet.startingTime);
+
+    const columns = [{
+        dataField: 'day',
+        text: 'Week Day'
+      },
+    {
+        dataField: 'date',
+        text: 'Date'
+    },
+    {
+        dataField: 'startingTime',
+        text: 'Starting Time',
+        formatter: ()=> (
+            <form noValidate>
+                <TextField
+                    id="time"
+                    type="time"
+                    defaultValue="09:00"
+                    // onChange={(e)=>setTimesheet(e.target.value)}
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                    inputProps={{
+                    step: 300, // 5 min
+                    }}
+                />
+            </form>
+        )
+    },
+    {
+        dataField: 'endingTime',
+        text: 'Ending Time',
+        formatter: ()=> (
+            <form noValidate>
+                <TextField
+                    id="time"
+                    type="time"
+                    defaultValue="09:00"
+                    // onChange={(e)=>setTimesheet(e.target.value)}
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                    inputProps={{
+                    step: 300, // 5 min
+                    }}
+                />
+            </form>
+        )
+    },
+    {
+        dataField: 'Total Hour',
+        text: 'Total Hour',
+        formatter: totalHoursCalc
+    },
+    {
+        dataField: 'isFloatingDay',
+        text: 'Floating Day',
+    },
+    {
+        dataField: 'isHoliday',
+        text: 'Holiday',
+    },
+    {
+        dataField: 'isVacation',
+        text: 'Vacation',
+    },
+    ];
+
+    function editTimesheet(){
+        props.editTimesheet(timesheet);
+    }
+
+
     return (
-        <div>
-            <BootstrapTable keyField='id' data={ timesheet } columns={ columns } bordered={ false }/>
+        <div className={TimeSheetHomeCSS.container}>
+            <div>
+                <BootstrapTable keyField='id' data={ props.timesheet } columns={ columns } bordered={ false }/>
+            </div>
+            <div>
+                <Button variant="contained" color="primary" onClick={editTimesheet}>
+                    Save
+                </Button>
+            </div>
         </div>
     )
 }
+
+const mapStateToProps = (state)=>{
+    return {
+        timesheet: state.curr_timeSheet
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        editTimesheet: (payload) => dispatch({type: actionTypes.EDIT_TIMESHEET, payload})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimeSheetHome);
