84e803dba6a60bfa0f04d035752c96658f5d66aa
roxyluoxiaofei <roxyluoxiaofei@gmail.com>
2021-08-11 13:16:02 -0700

    Update summaris.js

--- timesheet-frontend/src/components/Summary/summaris.js
+++ timesheet-frontend/src/components/Summary/summaris.js
@@ -2,10 +2,9 @@ import { React, Component, useState, useEffect} from 'react';
 import { connect } from 'react-redux';
 import { getTimesheetSummary } from '../../action/action'
 import { Link } from 'react-router-dom';
-import InfoIcon from './infoIcon';
 import { FaInfoCircle } from 'react-icons/fa';
-import ReactTooltip from "react-tooltip";
 import { convertISO_to_Date } from '../../services/dateConverter';
+import { Tooltip } from '@material-ui/core';
 
 
 class Summaries extends Component {
@@ -18,14 +17,13 @@ class Summaries extends Component {
             tableSize: 3,
             showAll: false,
             showSubmissionTag: false,
-            showCommentTag : false
         }
     }
 
     async componentDidMount() {
         await this.props.getTimesheetSummary()
         const initialList = await this.props.timesheetSummaries
-        this.setState({currentWeeklyTimesheets : initialList.slice(0, this.state.tableSize)});
+        this.setState({ currentWeeklyTimesheets: initialList.slice(0, this.state.tableSize) });      
     }
 
     changeToShowMoreOrLess = () => {
@@ -44,26 +42,27 @@ class Summaries extends Component {
         return "Show More"
     }
 
-    optionTags = (args) => {
-        console.log("week  " + args.week)
-        let selectedWeek = args.week.weekEnding
-        let option = args.week.weeklyTimesheets.approvedStatus == "Approved" ? "view" : "edit"
+    optionTags = (week) => {
+        let selectedWeek = convertISO_to_Date(week.weeklyTimesheets.weekEnding)
+        let option = week.weeklyTimesheets.approvedStatus == "Approved" ? "view" : "edit"
         let url = "timesheet?weekEnding="+ selectedWeek + '/' + option;
         return <Link to={ url}>{ option}</Link>
     }
 
-    setIfShowSubmissionStatusTag = (showHover) => {
-        console.log("showHover?")
-        this.setState({showSubmissionTag : showHover});
+
+    tagTextSubmission = (week) => {
+        const status = week.weeklyTimesheets.submissionStatus; 
+        const fileType = week.weeklyTimesheets.document.type;
+        if (status != 'Not Started' && fileType != 'Approved') {
+            return 'Items due: Proof of Approved TimeSheet'
+        } 
+        return 'Approval denied by Admin, please contact your HR manager'
     }
 
-    showInfoIcon = (args) => {
-        if (args.status == "Incomplete")
-            return <FaInfoCircle data-tip data-for="submissionTip"
-                        onMouseEnter={() => this.setIfShowSubmissionStatusTag(true)}
-                        onMouseLeave={() => this.setIfShowSubmissionStatusTag(false)}
-        />
+    showComment = (week) => {
+        
     }
+
 
     render() {
         
@@ -89,19 +88,20 @@ class Summaries extends Component {
                                 <td>{convertISO_to_Date(week.weeklyTimesheets.weekEnding)}</td>
                                 <td>{week.weeklyTimesheets.totalBillingHours}</td>
                                 <td>
-                                    {week.weeklyTimesheets.submissionStatus}<span> </span>
-                                    {this.showInfoIcon({ status: week.weeklyTimesheets.submissionStatus })}
-                                    <ReactTooltip id="submissionTip" place="top" effect="solid">
-                                        This is submission tip
-                                    </ReactTooltip>
+                                    <span>{week.weeklyTimesheets.submissionStatus}</span> <span></span>
+                                    {week.weeklyTimesheets.submissionStatus == "Incomplete" &&
+                                        <Tooltip title={this.tagTextSubmission(week)}>
+                                            <span>
+                                                <FaInfoCircle />
+                                            </span> 
+                                        </Tooltip>
+                                        }
+                                    
                                 </td>
                                 <td>{week.weeklyTimesheets.approvedStatus}</td>
-                                <td>{this.optionTags({ week: week })}</td>
-                                <td>{week.comment}
-                                    {this.showInfoIcon({ status: week.weeklyTimesheets.submissionStatus })}
-                                    <ReactTooltip id="submissionTip" place="top" effect="solid">
-                                        This is submission tip
-                                    </ReactTooltip>
+                                <td>{this.optionTags(week)}</td>
+                                <td>{this.showComment(week)}
+
                                 </td>
                             </tr>
                         
@@ -109,8 +109,7 @@ class Summaries extends Component {
                     </tbody>
                 </table>
                 <div className="text-center">
-                    {<button type="button" className="btn btn-light" onClick={() => this.changeToShowMoreOrLess()}>{this.showMoreTag()}</button>}
-                    
+                    {this.state.currentWeeklyTimesheets.length!=0&&<button type="button" className="btn btn-light" onClick={() => this.changeToShowMoreOrLess()}>{this.showMoreTag()}</button>}
                 </div>
                 
                 
@@ -120,15 +119,12 @@ class Summaries extends Component {
     
 }
 const mapStateToProps = (state) => {
-    console.log("mapStateToProps called")
-    console.log(state.summaryTimesheets)
     return {
         timesheetSummaries: state.summaryTimesheets
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    console.log("mapDispatchToProps called")
     return {
         getTimesheetSummary: () => dispatch(getTimesheetSummary())
     }
